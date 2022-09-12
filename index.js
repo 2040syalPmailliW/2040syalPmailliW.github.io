384c23fbc9b043a18a7feca72d17bce217e34e5c
2040syalPmailliW <104904805+2040syalPmailliW@users.noreply.github.com>
2022-09-12 13:19:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,35 @@ setInterval(() => {
     document.getElementById('cps').innerHTML = parseInt(clicks);
 }, 50);
 
+!function() {
+    function detectDevTool(allow) {
+      if(isNaN(+allow)) allow = 100;
+      var start = +new Date(); // Validation of built-in Object tamper prevention.
+      debugger;
+      var end = +new Date(); // Validates too.
+      if(isNaN(start) || isNaN(end) || end - start > allow) {
+        // input your code here when devtools detected.
+      }
+    }
+    if(window.attachEvent) {
+      if (document.readyState === "complete" || document.readyState === "interactive") {
+          detectDevTool();
+        window.attachEvent('onresize', detectDevTool);
+        window.attachEvent('onmousemove', detectDevTool);
+        window.attachEvent('onfocus', detectDevTool);
+        window.attachEvent('onblur', detectDevTool);
+      } else {
+          setTimeout(argument.callee, 0);
+      }
+    } else {
+      window.addEventListener('load', detectDevTool);
+      window.addEventListener('resize', detectDevTool);
+      window.addEventListener('mousemove', detectDevTool);
+      window.addEventListener('focus', detectDevTool);
+      window.addEventListener('blur', detectDevTool);
+    }
+  }();
+
 // function countTime(oldDate) {
 //     let difference = new Date() - oldDate
 //     return difference;
